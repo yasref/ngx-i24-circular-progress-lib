f21c06d37f0e9b27db61f721a4de39c17059affd
Yaseen Alrefaee <yaseenref@gmail.com>
2022-06-30 11:58:14 +0300

    NgxI24CircularProgressComponent

--- projects/ngx-i24-circular-progress/src/lib/ngx-i24-circular-progress.component.ts
+++ projects/ngx-i24-circular-progress/src/lib/ngx-i24-circular-progress.component.ts
@@ -1,19 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'ngx-i24-circular-progress',
-  template: `
-    <p>
-      ngx-i24-circular-progress works!
-    </p>
-  `,
+  templateUrl: `./ngx-i24-circular-progress.component.html`,
   styleUrls: ['./ngx-i24-circular-progress.component.scss']
 })
-export class NgxI24CircularProgressComponent implements OnInit {
+export class NgxI24CircularProgressComponent implements OnChanges {
+  @Input() option: I24CircularProgressOptions = {
+    primaryColor: '#0495fc',
+    secondaryColor: '#191919',
+    percentColor: '#000',
+    textColor: '#000000bf',
+    percent: 50,
+    text: 'css',
+    circleRadius: 70
+  };
+  size = '150px';
+  steps = 440;
+  ngOnChanges() {
+    this.option.primaryColor = this.option.primaryColor ? this.option.primaryColor : '#0495fc';
+    this.option.secondaryColor = this.option.secondaryColor ? this.option.secondaryColor : '#191919';
+    this.option.percentColor = this.option.percentColor ? this.option.percentColor : '#000';
+    this.option.textColor = this.option.textColor ? this.option.textColor : '#000000bf';
 
-  constructor() { }
+    this.option.circleRadius = this.option.circleRadius ? this.option.circleRadius : 70;
 
-  ngOnInit(): void {
+    this.size = ((this.option.circleRadius * 2) + 10) + 'px';
+    this.steps = Math.round(6.2857 * this.option.circleRadius);
   }
-
+  // get size() {
+  //   return ((this.option.circleRadius * 2) + 10) + 'px';
+  // }
+  // get steps() {
+  //   return Math.round(6.2857 * this.option.circleRadius);
+  // }
+}
+export interface I24CircularProgressOptions {
+  primaryColor?: string
+  secondaryColor?: string
+  percentColor?: string
+  textColor?: string
+  percent: number
+  text?: string
+  circleRadius?: number
 }
--- projects/ngx-i24-circular-progress/src/lib/ngx-i24-circular-progress.module.ts
+++ projects/ngx-i24-circular-progress/src/lib/ngx-i24-circular-progress.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NgxI24CircularProgressComponent } from './ngx-i24-circular-progress.component';
 
@@ -8,6 +9,7 @@ import { NgxI24CircularProgressComponent } from './ngx-i24-circular-progress.com
     NgxI24CircularProgressComponent
   ],
   imports: [
+    CommonModule
   ],
   exports: [
     NgxI24CircularProgressComponent
